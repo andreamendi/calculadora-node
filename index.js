bebfd489939efea0f1f0f3f1dfaa1765f8154c8c
AndreaMendi <andreamendi@icloud.com>
2019-02-08 20:52:44 -0600

    Calculadora: suma, resta, división y multiplicación v1

---
+++ index.js
@@ -0,0 +1,13 @@
+const math = require('./math');
+
+sum = math.suma(10,20);
+console.log("Tu suma es " + sum)
+
+res = math.resta(10,20);
+console.log("Tu resta es " + res)
+
+mul = math.mult(10,20);
+console.log("Tu multiplicación es " + mul)
+
+div = math.divi(10,20);
+console.log("Tu división es " + div)
---
+++ math.js
@@ -0,0 +1,20 @@
+function suma(a, b){
+    return a + b;
+
+}
+
+module.exports = {
+   suma: function (a, b){
+        return a + b;
+    },
+    resta: function(a, b){
+        return a - b
+    },
+    mult: function (a, b){
+        return a * b;
+    },
+    divi: function (a, b){
+        return a / b;
+    },
+}
+
